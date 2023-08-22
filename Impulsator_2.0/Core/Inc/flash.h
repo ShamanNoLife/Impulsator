5ae782263e5f24afc62966e359816885360065e1
ShamanNoLife <137199477+ShamanNoLife@users.noreply.github.com>
2023-08-22 16:17:51 +0200

    Update flash.h

--- Impulsator_2.0/Core/Inc/flash.h
+++ Impulsator_2.0/Core/Inc/flash.h
@@ -10,17 +10,8 @@
 
 #include "main.h"
 
-struct data_for_flash{
-	uint32_t data1;
-	uint32_t data2;
-	uint32_t data3;
-	uint32_t data4;
-	uint32_t data5;
-	uint32_t data6;
-};
-
 void save_data_1(uint32_t Adrr, uint32_t data);
-void save_data_2(uint32_t Adrr, uint32_t data1,uint32_t data2,uint32_t data3,uint32_t data4,uint32_t data5,uint32_t data6);
+void save_data_2(uint32_t Adrr, uint32_t* data);
 uint32_t read_data(uint32_t Address);
 void erase_data(uint32_t Address);
 #endif /* INC_FLASH_H_ */
