6ff53a488938e4ab5a050698765b6d714ed22405
ShamanNoLife <137199477+ShamanNoLife@users.noreply.github.com>
2023-08-22 16:17:19 +0200

    Update flash.c

--- Impulsator_2.0/Core/Src/flash.c
+++ Impulsator_2.0/Core/Src/flash.c
@@ -16,23 +16,11 @@ void save_data_1(uint32_t Adrr,uint32_t data){
 	HAL_FLASH_Lock();
 }
 
-void save_data_2(uint32_t Adrr, uint32_t data1,uint32_t data2,uint32_t data3,uint32_t data4,uint32_t data5,uint32_t data6){
-
-	struct data_for_flash data_struct;
-
-	data_struct.data1=data1;
-	data_struct.data2=data2;
-	data_struct.data3=data3;
-	data_struct.data4=data4;
-	data_struct.data5=data5;
-	data_struct.data6=data6;
-
-	uint32_t* ptr = (uint32_t*)&data_struct;
-
+void save_data_2(uint32_t Adrr, uint32_t* data){
     HAL_FLASH_Unlock();
 
 	for(int i =0;i<6;i++){
-		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD,Adrr+i*16,*(ptr+i));
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD,Adrr+i*4,*(data+i));
 	}
 
 	HAL_FLASH_Lock();
